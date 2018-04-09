367c4296e00a49c628f82b282898fae337087436
Willie <vip_willie@sina.com>
2018-04-09 18:01:47 +0800

    测试引入fastjson

--- src/main/java/com/willie/cloud/vod/controller/video/VodPortalController.java
+++ src/main/java/com/willie/cloud/vod/controller/video/VodPortalController.java
@@ -2,10 +2,12 @@ package com.willie.cloud.vod.controller.video;
 
 import com.willie.cloud.vod.constent.vod.Vod;
 import com.willie.cloud.vod.domain.config.CloudVodConfig;
+import com.willie.cloud.vod.domain.video.Video;
 import com.willie.cloud.vod.service.vod.CloudVodQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>功能 描述:</p>
@@ -27,6 +29,16 @@ public class VodPortalController {
         } else {
             return "redirect:/video/videos";//腾讯首页
         }
+    }
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public Video test() {
+        Video video = new Video();
+        video.setId(15);
+        video.setAppId("0111");
+        video.setVideoId("2254");
+        return video;
     }
 
     @Autowired
